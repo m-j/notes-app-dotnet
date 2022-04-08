11d97aa4024be0d5d330721f21bcaa84c8bd94ff
mathix <mathixx@gmail.com>
2022-04-08 08:46:00 +0200

    Adding tags controller

---
+++ NotesAppDotnet/NotesAppDotnet/Controllers/TagsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NotesAppDotnet.Model;
+
+namespace NotesAppDotnet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TagsController : ControllerBase
+{
+    private readonly IOptions<Settings> _settings;
+
+    public TagsController(IOptions<Settings> settings)
+    {
+        _settings = settings;
+    }
+
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return _settings.Value.Tags;
+    }
+}
