10c8bd712a2525f9a00f4975178e54ba7a08c815
Marcin Widmanski <marcinw@ntropy.pl>
2022-05-23 09:37:23 +0200

    Add OpenTelemetry metrics endpoint and instrumentation libs

--- NotesAppDotnet/NotesAppDotnet/Program.cs
+++ NotesAppDotnet/NotesAppDotnet/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NotesAppDotnet.Data;
 using NotesAppDotnet.Model;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,11 +18,24 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Host.UseSystemd();
 
+// https://opentelemetry.io/docs/instrumentation/net/automatic/
+builder.Services.AddOpenTelemetryMetrics(otel =>
+{
+    // Add instrumentation - metrics providers
+    // CPU / Memory  + GC activity aka Runtime Metrics
+    otel.AddRuntimeMetrics();
+    // HTTP Server call durations per return code
+    otel.AddAspNetCoreInstrumentation();
+
+    // Register Prometheus as OTEL exporter
+    otel.AddPrometheusExporter();
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
